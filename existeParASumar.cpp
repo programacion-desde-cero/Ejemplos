7d40ceb1d7bdf79ba720161c95ff7d4d0e62f362
Programación Desde Cero <54087641+programacion-desde-cero@users.noreply.github.com>
2020-08-02 21:35:45 -0300

    Explicación del código en https://youtu.be/nADemX9stHY
    "Dado un arreglo de enteros ordenados de forma ascendente, hallar un par de ellos cuya suma coincida con otro número, también dado."

---
+++ existeParASumar.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+bool existeParASumar(const vector<int> &numeros, int resultado) {
+	int inferior = 0;
+	int superior = numeros.size()-1;
+	while (inferior < superior) {
+		int s = numeros[inferior] + numeros[superior];
+		if (s == resultado)
+			return true;
+		if (s < resultado)
+		    inferior+=1;
+		else
+		    superior-=1;
+	}
+	return false;
+}
+
+
+int main() {
+    vector<int> a={1, 2, 3, 9};
+    if (existeParASumar(a, 8))
+        cout << "existe" << endl;
+    else
+        cout << "no existe" << endl;   
+    return 0;
+}
