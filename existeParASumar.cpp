c37e77c3c6e2a2f5e0ad1d82c4783c4b01891a20
Programación Desde Cero <54087641+programacion-desde-cero@users.noreply.github.com>
2021-03-07 11:31:00 -0300

    Update existeParASumar.cpp

--- existeParASumar.cpp
+++ existeParASumar.cpp
@@ -1,5 +1,5 @@
 /*
-Dado un arreglo de números ordenados de forma ascendente, hallar un par de ellos cuya suma coincida con otro número, también dado. 
+Dado un arreglo de números ordenados de forma ascendente, decir si existe un par de ellos cuya suma coincida con otro número, también dado. 
 Video explicativo: https://www.youtube.com/watch?v=nADemX9stHY
 */
 
